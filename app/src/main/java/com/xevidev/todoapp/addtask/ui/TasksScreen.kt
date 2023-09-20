cd0c365b738c84c4df55e2c1810f9d25b7d0ba79
XeviDev <jv.martinez.mellado@gmail.com>
2023-09-20 21:50:13 +0200

    Added DI

--- app/src/main/java/com/xevidev/todoapp/addtask/ui/TasksScreen.kt
+++ app/src/main/java/com/xevidev/todoapp/addtask/ui/TasksScreen.kt
@@ -2,7 +2,7 @@ package com.xevidev.todoapp.addtask.ui
 
 import androidx.compose.runtime.Composable
 
-class TasksScreen {
+class TasksScreen(tasksViewModel: TasksViewModel) {
     @Composable
     fun TasksScreen(){
 
