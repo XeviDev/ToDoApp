d3013cd7569da53c97eb4743878f1edf191f78d9
XeviDev <jv.martinez.mellado@gmail.com>
2023-09-29 17:02:55 +0200

    repository layer created

---
+++ app/src/main/java/com/xevidev/todoapp/addtask/data/TaskRepository.kt
@@ -0,0 +1,23 @@
+package com.xevidev.todoapp.addtask.data
+
+import com.xevidev.todoapp.addtask.ui.model.TaskModel
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Singleton
+class TaskRepository @Inject constructor(private val taskDAO: TaskDAO) {
+
+    val tasks: Flow<List<TaskModel>> =
+        //A map its like a forEach but it returns the list that iterate
+        taskDAO.getTasks().map { items ->
+            items.map {
+                TaskModel(it.id, it.task, it.selected)
+            }
+        }
+
+    suspend fun add(taskModel: TaskModel) {
+        taskDAO.addTask(TaskEntity(taskModel.id, taskModel.task, taskModel.selected))
+    }
+}
--- app/src/main/java/com/xevidev/todoapp/addtask/ui/model/TaskModel.kt
+++ app/src/main/java/com/xevidev/todoapp/addtask/ui/model/TaskModel.kt
@@ -4,7 +4,7 @@ package com.xevidev.todoapp.addtask.ui.model
 // Add the time in milis to create an unique id
 data class TaskModel(
     //We dont need to add the first arg because we already give it when create a TaskModel
-    val id: Long = System.currentTimeMillis(),
+    val id: Int = System.currentTimeMillis().hashCode(),//Transform the milis into a code
     val task: String,
     var selected: Boolean = false
 )
