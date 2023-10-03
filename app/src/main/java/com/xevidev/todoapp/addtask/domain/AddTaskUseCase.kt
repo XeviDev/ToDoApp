d9d2d46fc897fa0c5f5541682701cf4a03b4dd9b
XeviDev <jv.martinez.mellado@gmail.com>
2023-10-03 23:18:24 +0200

    Update AddTaskUseCase.kt

--- app/src/main/java/com/xevidev/todoapp/addtask/domain/AddTaskUseCase.kt
+++ app/src/main/java/com/xevidev/todoapp/addtask/domain/AddTaskUseCase.kt
@@ -5,9 +5,9 @@ import com.xevidev.todoapp.addtask.ui.model.TaskModel
 import javax.inject.Inject
 
 class AddTaskUseCase @Inject constructor(private val taskRepository: TaskRepository) {
- 
+
     suspend operator fun invoke(taskModel: TaskModel){
         taskRepository.add(taskModel)
     }
-
+ 
 }
